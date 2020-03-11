12dc4f4bd9e51c2b2f800629d15f2e52414b7c55
MuchlasS <muchlas148@gmail.com>
2020-03-11 14:16:23 +0700

    navbar + menghilangkan template

---
+++ components/navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar
+      :collapse="!collapseOnScroll"
+      :collapse-on-scroll="collapseOnScroll"
+      absolute
+      color="#3f51b5"
+      dark
+      scroll-target="#scroll"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Collapsing Bar</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      collapseOnScroll: true,
+    }),
+  }
+</script>
